f90548fcb834c38571a0fd2aa13bf02f211b4c1e
unknown <shekinahmanyi@gmail.com>
2022-07-26 05:38:49 +0100

    other changes

--- ninja_quiz/app.js
+++ ninja_quiz/app.js
@@ -1,5 +1,8 @@
 const correctAnswers = ['B','B','B','B'];
 const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+
 form.addEventListener('submit',e => {
     e.preventDefault();
 
@@ -11,7 +14,23 @@ if(answer === correctAnswers[index]){
     score += 25;
 }
 });
+//show results on page
+scrollTo(0,0);
+result.querySelector('span').textContent = `${score}%`;
+result.classList.remove('d-none');
 
-console.log(score);
+let output = 0;
+const timer = setInterval(() =>{
+result.querySelector('span').textContent = `${output}%`;
+if(output === score){
+    clearInterval(timer);
+}else{
+    output++;
+}
+}, 10);
 
 });
+
+
+
+
